bb5728bf05d5153d00d4251b241b6323341f771d
OatmealDome <julian@oatmealdome.me>
2021-05-14 01:05:18 -0400

    Aws: Add support for alternative ServiceURLs Rebase of loic-sharma/BaGet#272.

--- src/BaGet.Aws/AwsApplicationExtensions.cs
+++ src/BaGet.Aws/AwsApplicationExtensions.cs
@@ -33,7 +33,8 @@ namespace BaGet
 
                 var config = new AmazonS3Config
                 {
-                    RegionEndpoint = RegionEndpoint.GetBySystemName(options.Region)
+                    RegionEndpoint = options.Region != null ? RegionEndpoint.GetBySystemName(options.Region) : null,
+                    ServiceURL = options.ServiceUrl
                 };
 
                 if (options.UseInstanceProfile)
